751f2bb848a84010d2f1afd674ca8c30b4b9aa8f
Dmitri Mendis <2dmendis@gmail.com>
2018-06-12 04:05:25 +0000

    automatically backed up by learn

---
+++ lib/board.rb
@@ -0,0 +1,23 @@
+class Board 
+  attr_accessor :cells
+  
+  def initialize
+    
+  end 
+  
+  def reset! 
+    
+  end 
+  
+  def display 
+    
+  end 
+  
+  def update 
+    
+  end 
+  
+  def token 
+    
+  end 
+end 
